4c28bdac90ba6c17cbbbc04ce0b1f18dc080df9c
Newtonian1 <ross3jh@gmail.com>
2022-11-01 15:41:12 -0400

    added swipe interface component

---
+++ client/src/pages/swipe/swipeInterface.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function swipeInterface() {
+    return (
+        <div className="swipe-interface">
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+export default swipeInterface;
